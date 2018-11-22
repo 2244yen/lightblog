054b67e56b6ee282b7303a042a630e6e97ad39ad
linhdang <linhdtt92@gmail.com>
2018-11-22 20:41:18 +0700

    build client

--- server/routes/tag.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const tagCtrl = require('../controllers/tagController')
-const multipart = require('connect-multiparty')
-const multipartWare = multipart()
-
-// get articles
-router.get('/', tagCtrl.getAll)
-
-module.exports = router
